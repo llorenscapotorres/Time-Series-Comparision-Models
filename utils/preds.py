0f379619b5ccb92820fe75ca34fc57d109246cd9
Llorens <llorenscapo2001@gmail.com>
2025-09-19 18:08:00 +0200

    N-BEATSx finishied and implementing validation for evaluate better

--- utils/preds.py
+++ utils/preds.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+from darts import TimeSeries
+from darts.dataprocessing.transformers import Scaler
 
 def make_preds(model, input_data):
     """
@@ -27,3 +30,77 @@ def evaluate_preds(y_true, y_pred):
         'rmse': rmse,
         'mape': mape
     }
+
+def make_predictions_rolling_one_darts(ts_train: TimeSeries, 
+                                       ts_test: TimeSeries, 
+                                       ts_exogenus_train: TimeSeries, 
+                                       ts_exogenus_test: TimeSeries,
+                                       scaled: bool, 
+                                       scaler: Scaler, 
+                                       model):
+    current_series = ts_train
+    current_exogenus = ts_exogenus_train
+    preds = []
+
+    exo_values = ts_exogenus_test.values() if ts_exogenus_test is not None else [None] * len(ts_test)
+    for value, value_exo in zip(ts_test.values(), exo_values):
+
+        if ts_exogenus_test is not None:
+            next_time_cov = current_exogenus.end_time() + current_exogenus.freq
+            value_exo_ts = TimeSeries.from_times_and_values(
+                times=pd.DatetimeIndex([next_time_cov], freq=current_exogenus.freq),
+                values=value_exo.reshape(1, -1)
+            )
+            current_exogenus = current_exogenus.append(value_exo_ts)
+
+        pred = model.predict(n=1, series=current_series,
+                             future_covariates = current_exogenus if ts_exogenus_test is not None else None)
+        if scaled:
+            pred = scaler.inverse_transform(pred)
+        preds.append(pred.values()[0][0])
+
+        next_time = current_series.end_time() + current_series.freq
+        value_ts = TimeSeries.from_times_and_values(
+            times=pd.DatetimeIndex([next_time], freq=current_series.freq),
+            values=[[value]]
+        )
+        current_series = current_series.append(value_ts)
+
+    return preds
+
+def make_predictions_rolling_one_past_covariates_darts(ts_train: TimeSeries, 
+                                       ts_test: TimeSeries, 
+                                       ts_exogenus_train: TimeSeries, 
+                                       ts_exogenus_test: TimeSeries,
+                                       scaled: bool, 
+                                       scaler: Scaler, 
+                                       model):
+    current_series = ts_train
+    current_exogenus = ts_exogenus_train
+    preds = []
+
+    exo_values = ts_exogenus_test.values() if ts_exogenus_test is not None else [None] * len(ts_test)
+    for value, value_exo in zip(ts_test.values(), exo_values):
+
+        pred = model.predict(n=1, series=current_series,
+                             past_covariates = current_exogenus if ts_exogenus_test is not None else None)
+        if scaled:
+            pred = scaler.inverse_transform(pred)
+        preds.append(pred.values()[0][0])
+
+        next_time = current_series.end_time() + current_series.freq
+        value_ts = TimeSeries.from_times_and_values(
+            times=pd.DatetimeIndex([next_time], freq=current_series.freq),
+            values=[[value]]
+        )
+        current_series = current_series.append(value_ts)
+
+        if ts_exogenus_test is not None:
+            next_time_cov = current_exogenus.end_time() + current_exogenus.freq
+            value_exo_ts = TimeSeries.from_times_and_values(
+                times=pd.DatetimeIndex([next_time_cov], freq=current_exogenus.freq),
+                values=value_exo.reshape(1, -1)
+            )
+            current_exogenus = current_exogenus.append(value_exo_ts)
+
+    return preds
