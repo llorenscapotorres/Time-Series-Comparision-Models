1290ea0b8f724987431ade7df822c61641fd269e
Llorens <llorenscapo2001@gmail.com>
2025-10-07 20:47:13 +0200

    GRU week-horizon

--- utils/preds.py
+++ utils/preds.py
@@ -178,3 +178,45 @@ def make_predictions_rolling_one_nf(train_df: pd.DataFrame,
         df_current = pd.concat([df_current, new_row], ignore_index=True)
 
     return [df.iloc[0, 2] for df in preds_tst]
+
+def make_predictions_darts_horizon(ts_train: TimeSeries, 
+                                       ts_test: TimeSeries, 
+                                       ts_exogenus_train: TimeSeries, 
+                                       ts_exogenus_test: TimeSeries,
+                                       scaled: bool, 
+                                       scaler: Scaler,
+                                       horizon: int, 
+                                       model):
+    current_series = ts_train
+    current_exogenus = ts_exogenus_train
+    preds = []
+
+    exo_values = ts_exogenus_test.values() if ts_exogenus_test is not None else [None] * len(ts_test)
+    limit = len(ts_test) - horizon
+    for i, (value, value_exo) in enumerate(zip(ts_test.values(), exo_values)):
+
+        if i >= limit:
+            break
+
+        pred = model.predict(n=horizon, series=current_series,
+                             past_covariates = current_exogenus if ts_exogenus_test is not None else None)
+        if scaled:
+            pred = scaler.inverse_transform(pred)
+        preds.append(pred.values())
+
+        next_time = current_series.end_time() + current_series.freq
+        value_ts = TimeSeries.from_times_and_values(
+            times=pd.DatetimeIndex([next_time], freq=current_series.freq),
+            values=[[value]]
+        )
+        current_series = current_series.append(value_ts)
+
+        if ts_exogenus_test is not None:
+            next_time_cov = current_exogenus.end_time() + current_exogenus.freq
+            value_exo_ts = TimeSeries.from_times_and_values(
+                times=pd.DatetimeIndex([next_time_cov], freq=current_exogenus.freq),
+                values=value_exo.reshape(1, -1)
+            )
+            current_exogenus = current_exogenus.append(value_exo_ts)
+
+    return preds
